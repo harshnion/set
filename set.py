3ec40646cb836d174498aab99679b94a4023efdb
harshnion <86431354+harshnion@users.noreply.github.com>
2021-07-08 23:52:44 +0530

    14_commit

---
+++ set.py
@@ -0,0 +1,10 @@
+# Today we learn about set 
+# it same as of list, dictionary, tuple 
+
+# These are some example of set
+s1 = set([1,2,3,4,5,6])
+print(s1)
+s2 = set([1,2,2,3,4,4,5,6,6])
+print(s2)
+s3 = set([3,4,5,6,6,6,1,1,2])
+print(s3)
